8b762237ecafd7ce12a76e901d97fe44052a10e0
Marcial Rodríguez <uborzz@gmail.com>
2017-07-15 13:46:31 +0200

    roll - random

--- radybot.py
+++ radybot.py
@@ -22,15 +22,50 @@ def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="CallBot Version Dofitos. /help para aiuda! ")
     
 def help(bot, update):
+    print(update.message)
     bot.send_message(chat_id=update.message.chat_id, text="CallBOT lista de comandos:"
                                                           "\n/cs : Call CS."
                                                           "\n/hots ó /hos : Call HotS."
                                                           "\n/joincs y /leavecs "
                                                           "\n/joinhots y /leavehots "
-                                                          "\n/flip : Coin flip")
-    
+                                                          "\n/flip : Coin flip"
+                                                          "\n/roll : valores random"
+                                                          "\n/helproll : help de /roll")
+
+
+def roll(bot, update):
+    try:
+        values = [0, 100]
+        params = update.message.text.split()
+        if len(params) == 1:
+            result = random.randint(0, 100)
+        else:
+            if len(params) == 2:
+                result = random.randint(0, int(params[1]))
+                values[1] = params[1]
+            elif len(params) == 3:
+                result = random.randint(int(params[1]), int(params[2]))
+                values[0] = params[1]
+                values[1] = params[2]
+            print(result)
+        user = update.message.from_user.first_name + " " + update.message.from_user.last_name
+        user = user.rstrip()
+        ttr = "Random en [{}, {}]".format(values[0], values[1]) + \
+              "\nResult ({}): {}!".format(user, result)
+        print(ttr)
+        bot.send_message(chat_id=update.message.chat_id, text=ttr)
+    except:
+        helproll(bot, update)
+
+def helproll(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Roll: utiliza numeros enteros: "
+                                                   "\n/roll - random entre 0 y 100"
+                                                   "\n/roll <max> - entre 0 y <max>"
+                                                   "\n/roll <min> <max> - adivina...")
+
+
 def flip(bot, update):
-    if random.randint(0,2):
+    if random.randint(0,1):
         result = "Headz!"
     else:
         result = "Tailz!"
@@ -73,6 +108,8 @@ def rm_user(uid, collection):
             break
     if found:
         db[collection].delete_many({'uid':uid})
+
+#def join(bot, update):
 
 def joincs(bot, update):
     collection = 'csgo' + str(update.message.chat_id)
@@ -123,8 +160,14 @@ dispatcher.add_handler(start_handler)
 
 start_handler = CommandHandler('help', help)
 dispatcher.add_handler(start_handler)
+start_handler = CommandHandler('helproll', helproll)
+dispatcher.add_handler(start_handler)
 
 start_handler = CommandHandler('flip', flip)
+dispatcher.add_handler(start_handler)
+start_handler = CommandHandler('roll', roll)
+dispatcher.add_handler(start_handler)
+start_handler = CommandHandler('random', roll)
 dispatcher.add_handler(start_handler)
 
 hots_handler = CommandHandler('hots', hots)
