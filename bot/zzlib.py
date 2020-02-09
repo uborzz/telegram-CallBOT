b6ec6a73de901dc6bd064fd6a5e9cb8abddc7630
uborzz <uborzz@gmail.com>
2020-02-09 17:35:04 +0100

    general lib format

--- bot/zzlib.py
+++ bot/zzlib.py
@@ -1,7 +1,7 @@
+from dataclasses import dataclass
+from typing import List
 
-from pyrogram import Client, Filters, Message, User
-from typing import List, Union
-from dataclasses import dataclass, asdict
+from pyrogram import Client, Message, User
 
 
 @dataclass
@@ -12,24 +12,27 @@ class MentionedUser:
 
 def extract_mentioned_users(client: Client, message: Message) -> List[MentionedUser]:
     """
-    Recibe un mensaje para sacar de él todas las menciones de usuario y devolver el ID y First Name,
-    tanto menciones directas (@coleguita) como text_mentions de usuarios sin username.
+    Recibe un mensaje para sacar de él todas las menciones de usuario y devolver
+    el ID y First Name, tanto menciones directas (@coleguita) como text_mentions de
+    usuarios sin username.
 
     :param client:      Pyrogram Client
     :param message:     Pyrogram Message object
     :return:            Dictionary with "uid" <int> and "fname" <str> fields.
     """
     users_mentioned = list()
-    for ent in message.entities:
-        if ent.type == "mention":
-            mention_in_message = message.text[ent.offset:ent.offset + ent.length]
+    for entity in message.entities:
+        if entity.type == "mention":
+            mention_in_message = message.text[
+                entity.offset : entity.offset + entity.length
+            ]
             username = mention_in_message[1:]  # removes @
             user = client.get_users(username)
+            users_mentioned.append(MentionedUser(uid=user.id, fname=user.first_name))
+        elif entity.type == "text_mention":
             users_mentioned.append(
-                MentionedUser(uid=user.id, fname=user.first_name))
-        elif ent.type == "text_mention":
-            users_mentioned.append(MentionedUser(
-                uid=ent.user.id, fname=ent.user.first_name))
+                MentionedUser(uid=entity.user.id, fname=entity.user.first_name)
+            )
     return users_mentioned
 
 
@@ -42,6 +45,7 @@ def get_user_name(user: User) -> str:
         return user.first_name
     else:
         return f"ID: {user.id}"
+
 
 def expand_commands(commands: List[str], bot_name: str):
     commands = list(commands)
