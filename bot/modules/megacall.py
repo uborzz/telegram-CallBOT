f9628edd06be0198817c460dadc713319e1d725b
uborzz <uborzz@gmail.com>
2020-02-09 17:34:27 +0100

    megacall format

--- bot/modules/megacall.py
+++ bot/modules/megacall.py
@@ -1,31 +1,36 @@
-
-from pyrogram import Client, Filters, Message
 from typing import List
 
-from bot import zzlib
+from pyrogram import Client, Filters, Message
+
+from bot.zzlib import MentionedUser, expand_commands
 
 
-def load(bot_client: Client, name: str):
-
+def load(bot_client: Client, name: str, megacall_text=""):
     def _expand_commands(commands: List[str]) -> List[str]:
-        return zzlib.expand_commands(commands=commands, bot_name=name)
+        return expand_commands(commands=commands, bot_name=name)
 
     @bot_client.on_message(Filters.command(_expand_commands(["megacall"])))
     def megacall(client: Client, message: Message):
-        print("megacall")
+        print("megacall!")
         chat_id = message.chat.id
         try:
-            participants = client.get_chat_members(chat_id, offset=0, limit=200)
-            print(participants)
-            users = [(user['user']['id'], user['user']['first_name']) for user in participants if
-                    user['user']['is_bot'] is False]
-            print(users)
-            mentions = ''.join(
-                ['<a href="tg://user?id={}">{}</a> '.format(user[0], user[1]) for user in users])
-            text = "FUCKING MEGACALL!!!\n" + mentions
-            client.send_message(message.chat.id, text, parse_mode="html")
+            members = client.get_chat_members(chat_id, offset=0, limit=200)
+            users = [
+                MentionedUser(uid=member.user.id, fname=member.user.first_name)
+                for member in members
+                if not member.user.is_bot
+            ]
+            mentions = "".join(
+                [
+                    f'<a href="tg://user?id={user.uid}">{user.fname}</a> '
+                    for user in users
+                ]
+            )
+            text = megacall_text + mentions
+            client.send_message(chat_id=chat_id, text=text, parse_mode="html")
         except ValueError:
             client.send_message(
-                message.chat.id, 'Megacall only works in groups!', parse_mode="html")
+                message.chat.id, "Megacall only works in groups!", parse_mode="html"
+            )
 
     print("Megacall module loaded.")
