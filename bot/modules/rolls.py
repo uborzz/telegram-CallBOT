795cbfeb30ea5e09b61f3b21662a815a4f775dd8
uborzz <uborzz@gmail.com>
2020-02-09 19:07:59 +0100

    rolls format

--- bot/modules/rolls.py
+++ bot/modules/rolls.py
@@ -20,31 +20,27 @@ def load(bot_client: Client, name: str, help_texts: HelpTexts):
     )
     def roll(client: Client, message: Message):
         try:
-            lower, upper = [0, 100]
             params = message.text.split()
-            result = None
+            lower, upper = [0, 100]
+
             if len(params) == 1:
                 result = random.randint(0, 100)
+            elif len(params) == 2:
+                upper = int(params[1])
+                result = random.randint(0, upper)
+            elif len(params) == 3:
+                lower = int(params[1])
+                upper = int(params[2])
+                if lower > upper:
+                    lower, upper = upper, lower
+                result = random.randint(lower, upper)
             else:
-                if len(params) == 2:
-                    result = random.randint(0, int(params[1]))
-                    upper = params[1]
-                elif len(params) == 3:
-                    lower = int(params[1])
-                    upper = int(params[2])
-                    if lower > upper:
-                        lower, upper = upper, lower
-                    result = random.randint(lower, upper)
-
-            if result is None:
                 help_texts(client, message.chat.id, "roll")
+                return
 
-            else:
-                user = zzlib.get_user_name(message.from_user)
-                text_to_return = (
-                    f"Random en [{lower}, {upper}]\nResult ({user}): {result}!"
-                )
-                client.send_message(chat_id=message.chat.id, text=text_to_return)
+            user = zzlib.get_user_name(message.from_user)
+            text_to_return = f"Random en [{lower}, {upper}]\nResult ({user}): {result}!"
+            client.send_message(chat_id=message.chat.id, text=text_to_return)
 
         except ValueError:
             help_texts(client, message.chat.id, "roll")
