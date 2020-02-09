89bbb2b67a202c8fff77c185f32454bd865e655a
uborzz <uborzz@gmail.com>
2020-02-09 17:31:33 +0100

    doom module format

--- bot/modules/doom.py
+++ bot/modules/doom.py
@@ -1,77 +1,87 @@
-
 from datetime import datetime, timedelta
-from pyrogram import Client, Filters, Message
-from typing import List
-from pymongo.collection import Collection
-from apscheduler.schedulers.background import BackgroundScheduler
 from random import randint
+from typing import List
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from pymongo.collection import Collection
+from pyrogram import Client, Filters, Message
 
 from bot import zzlib
 
+from .doom_dao import DoomDAO, DoomedUser
 
-def load(bot_client: Client, db: Collection, bot_name: str, scheduler: BackgroundScheduler):
 
-    def limpieza_expirados():
-        print("rutina limpia...")
+def load(
+    bot_client: Client,
+    collection: Collection,
+    bot_name: str,
+    scheduler: BackgroundScheduler,
+    doom_duration: int = 10,
+    doom_cooldown: int = 180,
+):
+
+    # loads its DAO
+    dao = DoomDAO(collection)
+
+    # configures its scheduled tasks
+    def cleansing():
+        print("running cleansin rutine...")
         time_now = datetime.now()
-        db.delete_many({'ts_reset': {'$gt': time_now}})
+        dao.clear(time_now)
 
-    scheduler.add_job(limpieza_expirados, "interval", minutes=10)
+    scheduler.add_job(cleansing, "interval", minutes=10)
 
+    # aux functs
     def _expand_commands(commands: List[str]) -> List[str]:
         return zzlib.expand_commands(commands=commands, bot_name=bot_name)
 
-    @bot_client.on_message(Filters.command(_expand_commands(["calla", "stfu"])))
+    @bot_client.on_message(Filters.command(_expand_commands(["doom", "calla", "stfu"])))
     def calla(client: Client, message: Message):
         chat_id = message.chat.id
-        try:
-            if message.reply_to_message:
-                user = message.reply_to_message.from_user
-                if user['is_bot']:
-                    client.send_message(
-                        message.chat.id, 'No le hagas /calla a un bot, cabrón.', parse_mode="html")
-                else:
-                    try:
-                        r = db.find_one(
-                            {"uid": user['id'], "first_name": user['first_name'], "chat_id": chat_id})
-                        if r:
-                            text = '<a href="tg://user?id={}">{}</a> '.format(
-                                user['id'], user['first_name'])
-                            text = text + " ya ha sido maldito recientemente... dale un fucking break, ok?"
-                        else:
-                            date_first_message = datetime.utcfromtimestamp(
-                                message.date)
-                            date_liberation = date_first_message + \
-                                timedelta(minutes=10)
-                            date_reset = date_first_message + timedelta(hours=3)
-                            db.insert_one({"uid": user['id'], "first_name": user['first_name'], "chat_id": chat_id,
-                                               "ts_doom": date_first_message, "ts_lib": date_liberation,
-                                               "ts_reset": date_reset})
-                            text = '<a href="tg://user?id={}">{}</a> '.format(
-                                user['id'], user['first_name'])
-                            text = text + " is DOOMED now!"
-                        client.send_message(
-                            message.chat.id, text, parse_mode="html")
-                    except Exception as e:
-                        print(e)
-            else:
-                client.send_message(
-                    message.chat.id, 'Usa /calla en reply a un mensaje.', parse_mode="html")
-        except ValueError as e:
-            print(e)
+        command = message.command[0]
 
+        if message.reply_to_message:
+            user = message.reply_to_message.from_user
+            if user.is_bot:
+                client.send_message(
+                    chat_id,
+                    f"No le hagas {command} a un bot, cabrón.",
+                    parse_mode="html",
+                )
+            else:
+                doomed_user: DoomedUser = dao.find(uid=user.id, chat_id=chat_id)
+                if doomed_user:
+                    text = f'<a href="tg://user?id={doomed_user.uid}">{doomed_user.first_name}</a> ya ha sido maldito recientemente... dale un fucking break, ok?'
+                else:
+                    date_first_message = datetime.utcfromtimestamp(message.date)
+                    doomed_user = DoomedUser(
+                        uid=user.id,
+                        first_name=user.first_name,
+                        chat_id=chat_id,
+                        ts_doom=date_first_message,
+                        ts_lib=date_first_message + timedelta(minutes=doom_duration),
+                        ts_reset=date_first_message + timedelta(minutes=doom_cooldown),
+                    )
+                    dao.doom(doomed_user)
+                    text = f'<a href="tg://user?id={user.id}">{user.first_name}</a> is DOOMED now!'
+                client.send_message(message.chat.id, text, parse_mode="html")
+
+        else:
+            client.send_message(
+                message.chat.id,
+                f"Usa /{command} en reply a un mensaje.",
+                parse_mode="html",
+            )
 
     @bot_client.on_message(~Filters.bot)
     def doom(_: Client, message: Message):
         time_now = datetime.utcnow()
         user = message.from_user
         chat_id = message.chat.id
-        r = db.find_one(
-            {"uid": user['id'], "first_name": user['first_name'], "chat_id": chat_id})
-        if r and time_now >= r['ts_reset']:
-            db.delete_one(
-                {"uid": user['id'], "first_name": user['first_name'], "chat_id": chat_id})
-        elif r and time_now <= r['ts_lib']:
+        doomed_user = dao.find(uid=user.id, chat_id=chat_id)
+        if doomed_user and time_now >= doomed_user.ts_reset:
+            dao.undoom(doomed_user)
+        elif doomed_user and time_now <= doomed_user.ts_lib:
             message_random = randint(1, 6)
             if message_random >= 6:
                 message.reply("QUE ME LEVANTO!", quote=True)
---
+++ bot/modules/doom_dao.py
@@ -0,0 +1,36 @@
+from dataclasses import asdict, dataclass
+from datetime import datetime
+
+from pymongo.collection import Collection
+
+
+@dataclass
+class DoomedUser:
+    uid: int
+    first_name: str
+    chat_id: int
+    ts_doom: datetime
+    ts_lib: datetime
+    ts_reset: datetime
+
+    @property
+    def to_dict(self):
+        return asdict(self)
+
+
+class DoomDAO:
+    def __init__(self, db: Collection):
+        self.db = db
+
+    def find(self, uid: int, chat_id: int) -> DoomedUser:
+        db_user = self.db.find_one({"uid": uid, "chat_id": chat_id}, {"_id": 0})
+        return DoomedUser(**db_user) if db_user else None
+
+    def doom(self, user: DoomedUser):
+        self.db.insert_one(user.to_dict)
+
+    def undoom(self, user: DoomedUser):
+        self.db.delete_one(user.to_dict)
+
+    def clear(self, time_now: datetime):
+        self.db.delete_many({"ts_reset": {"$gt": time_now}})
